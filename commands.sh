3571983d0378adb9bdc80c1a4793973bedc7f012
Jonatan Sundqvist <jonatan.sundqvist@vimla.se>
2020-10-11 04:43:36 +0200

    Add some reusable shell commands

--- commands.sh
+++ commands.sh
@@ -1,1 +1,12 @@
-python3 -m grpc_tools.protoc -I./ --python_out=. --grpc_python_out=. wgtwo/**/*.proto
+function generate-python {
+    python3 -m grpc_tools.protoc -I./ --python_out=. --grpc_python_out=. wgtwo/**/*.proto
+}
+
+function curl {
+    grpcurl \
+        -H "Authorization: Basic <base64token>"\
+        -import-path . \
+        -proto wgtwo/sms/v0/sms.proto \
+        api.wgtwo.com:443 \
+        wgtwo.sms.v0.SmsService/ReceiveText
+}
